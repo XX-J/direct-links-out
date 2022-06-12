451a1bde6c1bcb74d36d6cc2c13be6ca6296e952
XX-J <XX-J@mail.ru>
2022-06-12 12:42:12 +0300

    v3.0 beta - Many changes...
    Changed EventListener to MutationObserver.
    +Car.ru
    +Рамблер
    ...and many minor changes.

--- Direct links out.user.js
+++ Direct links out.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name            Direct links out
 // @name:ru         Прямые ссылки наружу
-// @version         2.92
+// @version         3.0
 // @description     Removes all "You are leaving our site..." and redirection stuff from links
 // @description:ru  Убирает "Бла-бла-бла, вы покидаете наш сайт" и переадресацию из ссылок
 // @icon            https://raw.githubusercontent.com/XX-J/Direct-links-out/master/icon.png
@@ -16,6 +16,9 @@
 //   Большой вопрос
 // @include         *://bolshoyvopros.ru/*
 // @include         *://*.bolshoyvopros.ru/*
+//   Car.ru
+// @include         *://car.ru/*
+// @include         *://*.car.ru/*
 //   DanielDefo
 // @include         *://danieldefo.ru/*
 // @include         *://*.danieldefo.ru/*
@@ -94,6 +97,9 @@
 //   PlayGround
 // @include         *://playground.ru/*
 // @include         *://*.playground.ru/*
+//   Рамблер
+// @include         *://rambler.ru/*
+// @include         *://*.rambler.ru/*
 //   Repack.me
 // @include         *://repack.me/*
 // @include         *://*.repack.me/*
@@ -135,8 +141,10 @@
 //   Wikimapia
 // @include         *://wikimapia.org/*
 //   Яндекс
-// @include         *://yandex.*
-// @include         *://*.yandex.*
+// @include         *://yandex.*/search/*
+// @include         *://*.yandex.*/search/*
+// @include         *://yandex.*/images/*
+// @include         *://*.yandex.*/images/*
 //   ЯПлакалъ
 // @include         *://yaplakal.com/*
 // @include         *://*.yaplakal.com/*
@@ -155,71 +163,35 @@ const Location = window.location, HostName = window.location.hostname;
 
 //   Simple rewrite link
 function rwSimple(link) {
-  if (RemoveAttributes) for (let c = 0; c < RemoveAttributes.length; c++) {
-    link.removeAttribute(RemoveAttributes[c]);
-  }
-  if (RemoveClasses) for (let c = 0; c < RemoveClasses.length; c++) {
-    link.classList.remove(RemoveClasses[c]);
-  }
-  if (/^http/i.test(link.href)) {
-    if (Anchor && ~link.href.search(Anchor)) {
+  if (RemoveAttributes) for (let RemoveAttribute of RemoveAttributes) link.removeAttribute(RemoveAttribute);
+  if (RemoveClasses) RemoveClasses.forEach( RemoveClass => link.classList.remove(RemoveClass));
+  if (/^((ht|f)tp|magnet|ed2k)/i.test(link.href)) {
+    if (Anchor && Anchor.test(link.href)) {
       link.href = decodeURIComponent(link.href).replace(Anchor, ReplacerAnchor);
       if (/^(aHR0c|ZnRw|bWFnbmV0|ZWQya)/.test(link.getAttribute('href'))) link.href = decodeURIComponent(escape(window.atob(link.getAttribute('href'))));
     }
-    if (link.href.search(After) > 0) link.href = decodeURIComponent(link.href).replace(After, ReplacerAfter);
+    if (After && After.test(link.href)) link.href = decodeURIComponent(link.href).replace(After, ReplacerAfter);
   }
 }
 
-function rwaSimple() {
-  let links = document.getElementsByTagName('a');
-  for (let c = 0; c < links.length; c++) rwLink(links[c]);
+let rwLink = rwSimple;
+
+let rwAll = () => {
+  for (let AnchorElement of document.querySelectorAll('a')) rwLink(AnchorElement);
+  new MutationObserver( Mutations => {
+    for (let Mutation of Mutations) {
+      for (let Node of Mutation.addedNodes) {
+        if (Node instanceof HTMLElement) {
+          if (Node instanceof HTMLAnchorElement) rwLink(Node);
+          for (let AnchorElement of Node.querySelectorAll('a')) rwLink(AnchorElement);
+        }
+      }
+    }
+  }).observe( document.body, { childList: true, subtree: true });
 }
 
 
-//   Facebook
-function rwFacebook(link) {
-  if (/referrer_log/i.test(link.onclick)) RemoveAttributes = ['onclick', 'onmouseover'];
-  rwSimple(link);
-}
-
-//   Google
-function rwGoogle(link) {
-  rwSimple(link);
-  // Images  --- get rid of setInterval
-  if (/&tbm=isch/i.test(Location)) setInterval (function() {
-    document.querySelector('[data-a] img').parentNode.href = document.querySelector('[data-a] img').src
-  }, 700);
-  // News  --- get rid of setInterval
-  if (/news/i.test(HostName) && link.hasAttribute('jslog')) setInterval (function() {
-    let jslog = link.getAttribute('jslog');
-    jslog = jslog.substring(jslog.indexOf(':') + 1, jslog.lastIndexOf(';')).replace(/(-|\.)/g, '+').replace(/_/g, '/');
-    jslog = unescape(window.atob(jslog).replace(/\\u/g, '%u'));
-    link.href = jslog.substring(jslog.indexOf('"') + 1, jslog.lastIndexOf('"'));
-  }, 700);
-}
-
-//   Kickass Torrents
-function rwKickassTorrents(link) {
-  if (!!link.getAttribute('href')) {
-    link.href = decodeURIComponent(link.href);
-    if (~link.href.search(Anchor)) rwSimple(link);
-  }
-}
-
-//   Ответы Mail.ru
-function rwOtvetyMailRu(link) {
-  if (/^((ht|f)tp|magnet|ed2k)/i.test(link.href) && !/^(ht|f)tps?:\/\/[^\/]*mail\.ru/i.test(link.href)) link.rel = "301";
-}
-
-//   Twitter
-function rwTwitter(link) {
-//  if (link.href.includes('/t.co/')) {   --- fetch ?
-  if (/^(ht|f)tp/i.test(link.text)) link.href = link.text.replace('…', '');
-}
-
-
-//   Determine Anchors, functions and listeners
-let rwLink = rwSimple, rwAll = rwaSimple;
+//   Determine anchors, functions and observers:
 
 if (/(4pda|instagram)/i.test(HostName)) {
   Anchor = /.+u=/i;  After = /&e=.*/i;
@@ -231,28 +203,67 @@ else if (/(adguard|github)/i.test(HostName)) {
 else if (/(bolshoyvopros|forumavia)/i.test(HostName)) {
   Anchor = /.+\?l=/i;  After = /&src=.*/i;
 }
+else if (/(^|\.)car\.ru$/i.test(HostName)) {
+  rwAll = () => {
+    function ChangeTagName(SelectedElement) {
+      let NewElement = document.createElement('a');
+      for (let Attribute of SelectedElement.attributes) NewElement.setAttribute(Attribute.name, Attribute.value);
+      let onclick = NewElement.getAttribute('onclick');
+      NewElement.setAttribute('href', onclick.slice(onclick.indexOf("href='") + 6, onclick.lastIndexOf("'")));
+      NewElement.removeAttribute('onclick');
+      NewElement.innerHTML = SelectedElement.innerHTML;
+      SelectedElement.parentNode.replaceChild(NewElement, SelectedElement);
+    }
+    for (let SelectedElement of document.querySelectorAll('[onclick*="href"]')) ChangeTagName(SelectedElement);
+    new MutationObserver( Mutations => {
+      for (let Mutation of Mutations) {
+        for (let Node of Mutation.addedNodes) {
+          if (Node instanceof HTMLElement) {
+            if (Node.matches('[onclick*="href"]')) ChangeTagName(Node);
+            for (let SelectedElement of Node.querySelectorAll('[onclick*="href"]')) ChangeTagName(SelectedElement);
+          }
+        }
+      }
+    }).observe( document.body, { childList: true, subtree: true });
+  }
+}
 else if (/danieldefo/i.test(HostName)) {
-  RemoveAttributes = ['data-proxy-href'];
+  rwLink = link => { link.removeAttribute('data-proxy-href') }
 }
 else if (/deviantart/i.test(HostName)) {
   Anchor = /.+outgoing\?/i;
 }
 else if (/disq/i.test(HostName)) {
-  Anchor = /.+url=/i;  After = /:[^\.]+$/;
+  Anchor = /.+url=/i;  After = /:[^\.]{9,}$/;
 }
-else if (/(electrotransport|fishki|liveinternet|oszone|pixiv|reactor|repack|rsload|soundcloud|steam|usbdev|wikimapia)/i.test(HostName)) {
+else if (/(electrotransport|fishki|liveinternet|oszone|pixiv|rambler|reactor|repack|rsload|soundcloud|steam|usbdev|wikimapia)/i.test(HostName)) {
   Anchor = /.+url=/i;
 }
 else if (/(facebook|messenger)/i.test(HostName)) {
   Anchor = /.+u=/i;  After = /(\?|&)(h|fbclid)=.*/i;
-  rwLink = rwFacebook;
+  rwLink = link => {
+    if (/referrer_log/i.test(link.onclick)) { link.removeAttribute('onclick'); link.removeAttribute('onmouseover') }
+    rwSimple(link);
+  }
 }
 else if (/ferra/i.test(HostName)) {
   Anchor = /.+click\/forums_out\//i;
 }
 else if (/google/i.test(HostName)) {
+//  if (/referrer_log/i.test(link.onclick)) for (let RemoveAttribute of ['onclick', 'onmouseover']) link.removeAttribute(RemoveAttribute);
   RemoveAttributes = ['data-jsarwt', 'data-usg', 'data-ved', 'jsname', 'jsaction'];
-  rwLink = rwGoogle;
+  rwLink = link => {
+    rwSimple(link);
+    //  Images
+    if (/&tbm=isch/i.test(Location)) document.querySelector('[data-a] img').parentNode.href = document.querySelector('[data-a] img').getAttribute('src');
+    //  News
+    if (/news/i.test(HostName) && link.hasAttribute('jslog')) {
+      let jslog = link.getAttribute('jslog');
+      jslog = jslog.slice(jslog.indexOf(':') + 1, jslog.lastIndexOf(';')).replace(/(-|\.)/g, '+').replace(/_/g, '/');
+      jslog = unescape(window.atob(jslog).replace(/\\u/g, '%u'));
+      link.href = jslog.slice(jslog.indexOf('"') + 1, jslog.lastIndexOf('"'));
+    }
+  }
 }
 else if (/ixbt/i.test(HostName)) {
   Anchor = /.+live\/redirect\//i;
@@ -260,10 +271,10 @@ else if (/ixbt/i.test(HostName)) {
 else if (/kickassto/i.test(HostName)) {
   RemoveAttributes = ['class'];
   Anchor = /.+confirm\/url\//i;
-  rwLink = rwKickassTorrents;
+  rwLink = link => { if (Anchor.test(link.href)) rwSimple(link) }
 }
 else if (/otvet\.mail\.ru/i.test(HostName)) {
-  rwLink = rwOtvetyMailRu;
+  rwLink = link => { if (/^((ht|f)tp|\/\/|magnet|ed2k)/i.test(link.getAttribute('href')) && !/(^|\.)mail\.ru$/i.test(link.hostname)) link.rel = "301" }
 }
 else if (/mozilla/i.test(HostName)) {
   Anchor = /.+outgoing.prod.mozaws.net\/v.\/[0-9a-zA-Z]+\//i;  After = /(\?|&)utm_content=.*/i;
@@ -281,10 +292,15 @@ else if (/(playground|rubattle)/i.test(HostName)) {
   Anchor = /www\.(playground\.ru|rubattle\.net)\/redirect\/(https?\/)?/i;
 }
 else if (/rutracker/i.test(HostName)) {
-  RemoveClasses = ['p-ext-link'];
+  rwAll = () => {
+    for (let AnchorElement of document.querySelectorAll('.p-ext-link')) AnchorElement.classList.remove('p-ext-link');
+    new MutationObserver( Mutations => {
+      for (let Mutation of Mutations) if (Mutation.target.classList.contains('p-ext-link')) Mutation.target.classList.remove('p-ext-link');
+    }).observe( document.body, { attributeFilter: ['class'], subtree: true });
+  }
 }
 else if (/slack/i.test(HostName)) {
-  RemoveAttributes = ['onclick', 'onmouseover'];
+  rwLink = link => { link.removeAttribute('onclick'); link.removeAttribute('onmouseover') }
 }
 else if (/taker/i.test(HostName)) {
   Anchor = /.+phpBB2\/goto\//i;
@@ -293,7 +309,17 @@ else if (/tumblr/i.test(HostName)) {
   Anchor = /.+redirect\?z=/i;  After = /&t=.*/i;
 }
 else if (/twitter/i.test(HostName)) {
-  rwLink = rwTwitter;
+  rwLink = link => {
+    if (link.href.includes('/t.co/')) {
+      if (/^((ht|f)tp|magnet|ed2k)/i.test(link.text)) { link.href = link.text.replace('…', ''); }
+//      не срабатывает, попробовать set interval:            Content Security Policy?
+//    else {
+//      console.log('111 Pre_fetch 111');
+//      link.href = (await (await fetch(link.href)).text()).replace(/.+\<title\>/i, '').replace(/\<\/title.+/i, '');
+//      console.log('222 Post_fetch 222');
+//    }
+    }
+  }
 }
 else if (/upwork/i.test(HostName)) {
   Anchor = /.+leaving-odesk\?ref=/i;
@@ -301,9 +327,11 @@ else if (/upwork/i.test(HostName)) {
 else if (/(vk|zoon)/i.test(HostName)) {
   Anchor = /.+to=/i;  After = /(\?|&)(cc_key|from_content|post|hash)=.*/i;
 }
-else if (/yandex/i.test(HostName)) {
-  RemoveAttributes = ['data-counter'];
-  Anchor = /.+&img_url=/i;  After = /&pos=.*/i;
+else if (/yandex\.[a-z]{1,4}\/search\//i.test(Location)) {
+  rwLink = link => { link.removeAttribute('data-counter') }
+}
+else if (/yandex\.[a-z]{1,4}\/images\//i.test(Location)) {
+  Anchor = /.+&img_url=/i;  After = /&text=.*/i;
 }
 else if (/yaplakal/i.test(HostName)) {
   Anchor = /.+go\/\?/i;
@@ -311,13 +339,5 @@ else if (/yaplakal/i.test(HostName)) {
 else if (/youtube/i.test(HostName)) {
   Anchor = /.+q=/i;  After = /&(redir_token|event|v)=.*/i;
 }
-
-document.addEventListener('DOMNodeInserted', function(event) {
-  if (!event || !event.target || !(event.target instanceof HTMLElement)) return;
-  let node = event.target;
-  if (node instanceof HTMLAnchorElement) rwLink(node);
-  let links = node.getElementsByTagName('a');
-  for (let c = 0; c < links.length; c++) rwLink(links[c]);
-}, false);
 
 rwAll();
